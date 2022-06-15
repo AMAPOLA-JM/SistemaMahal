42085aef619f7d1ce8f7ba771ea6479ee4acf282
Amapola-JM <raulmanu.27.03.2001@gmail.com>
2022-06-15 11:41:09 -0500

    Starting configure migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,14 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name_user');
+            $table->string('surname_user');
+            $table->int('DNI_user');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->boolean('type_user');
+            $table->boolean('state_user');
             $table->rememberToken();
             $table->timestamps();
         });
