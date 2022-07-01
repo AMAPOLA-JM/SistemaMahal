cfb75ed854f2b29a6be210403361285c105fcb5b
Amapola-JM <raulmanu.27.03.2001@gmail.com>
2022-07-01 02:21:31 -0500

    Autentification sistem insert

--- routes/web.php
+++ routes/web.php
@@ -66,3 +66,7 @@ Route::resources([
 Route::resources([
     'Users' => UsersController::class
 ]);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
