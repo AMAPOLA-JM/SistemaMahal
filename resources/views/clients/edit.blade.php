bc28bdf57aeedfe21894894f993539c521748126
Amapola-JM <raulmanu.27.03.2001@gmail.com>
2022-08-04 18:57:16 -0500

    Configurando Clients

--- resources/views/clients/edit.blade.php
+++ resources/views/clients/edit.blade.php
@@ -2,7 +2,7 @@
 @section('content_header')
 <div class="row">
     <div class="col-sm-10">
-        <h2>Editar Cliente</h2>
+        <h2>Nuevo Cliente</h2>
     </div>
     <div class="col-sm-2">
         <div class="input-group input-group-sm">
@@ -18,25 +18,26 @@
     <div class="col-sm-12">
         <div class="card">
             <div class="card-header bg-light">
-                <i class="fa fa-check"> Detalles de la Cliente</i>
+                <i class="fa fa-check"> Detalles de la Cliente: {{$client->id_client}}</i>
             </div>
-            <form class="" action="{{route('clients.store')}}" method="POST">
+            <form class="" action="{{route('clients.update')}}" method="POST">
                 <div class="card-body">
+                    <input type="hidden" name="id_client" value="{{$client->id_client}}">
                     <div class="form-group">
                         <label for="dni_client">DNI:</label>
-                        <input class="form-control" type="text" id="dni_client" name="dni_client" value="" required>
+                        <input class="form-control" type="text" id="dni_client" name="dni_client" value="{{$client->dni_client}}" required>
                     </div>
                     <div class="form-group">
                         <label for="name_client">Nombre:</label>
-                        <input class="form-control" type="text" id="name_client" name="name_client" value="" required>
+                        <input class="form-control" type="text" id="name_client" name="name_client" value="{{$client->name_client}}" required>
                     </div>
                     <div class="form-group">
                         <label for="surname_client">Apellido:</label>
-                        <input class="form-control" type="text" id="surname_client" name="surname_client" value="" required>
+                        <input class="form-control" type="text" id="surname_client" name="surname_client" value="{{$client->surname_client}}" required>
                     </div>
                     <div class="form-group">
                             <label for="tel_client">N° Celular:</label>
-                        <input class="form-control" type="number" id="tel_client" name="tel_client" value="" required>
+                        <input class="form-control" type="number" id="tel_client" name="tel_client" value="{{$client->tel_client}}" required>
                     </div>
                     @csrf
                     <div class="col-sm-12">
